c824a8db2a05fdc111a03493e71413bf66b28a69
annrra <annrra@gmail.com>
2025-08-08 09:53:00 +0300

    Projects section

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,16 @@
+import styles from './pr.module.css';
+
+const Projects = async () => {
+
+  return (
+    <section id="cases-heading" className={styles.cases}>
+      <div>
+        <h2>Case Studies</h2>
+        <p>Some of the projects I have been working on recently</p>
+      </div>
+    </section>
+  );
+
+};
+
+export default Projects;
