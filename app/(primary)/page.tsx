e80469503e47e38e203a52dafa081ca1a11be633
annrra <annrra@gmail.com>
2026-02-03 21:46:42 +0200

    Initialize clean updated Next.js project structure

---
+++ app/(primary)/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+//import { AccentBlock } from '@/src/components/AccentBlock';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      ...
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | BetterMonday',
+    default: 'BetterMonday',
+  },
+  description: 'Independent web designer & developer delivering custom websites, UI/UX design, and digital solutions to help brands stand out.',
+  metadataBase: new URL('https://bettermonday.org/'),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
