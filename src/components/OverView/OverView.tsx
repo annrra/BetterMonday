a6b9424c02688852bcc04ce1ba9d6e15995cd542
annrra <annrra@gmail.com>
2026-02-20 23:54:15 +0200

    Improve overview overlay animation timing and transitions

--- src/components/OverView/OverView.tsx
+++ src/components/OverView/OverView.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import OverViewPanel from './OverViewPanel';
 import OverviewLogoSvg from './OverviewLogoSvg';
 import styles from './ov.module.css';
@@ -10,31 +9,18 @@ const OverView = async () => {
 
   return (
     <OverViewPanel>
-      <div className={styles.pane}>
-        <Link href="/" className={styles.x} />
-        <div className={styles.body}>
-          <div className={styles.logo}>
-            <OverviewLogoSvg />
-          </div>
-          <div className={classNames(styles.feed, styles.feed01)}>
-            I am a UX designer / developer and occasional photographer from Bulgaria currently living in Sofia. Here at bettermonday can be found some of the stuff over which I've been working lately. My recent work consists mostly of design, Front-end development and its implementation into different content management systems.
-          </div>
-          <div className={classNames(styles.feed, styles.feed02)}>
-            I like to keep things simple mixing up thoughtful research and attention for the detail in the process. I'm currently available for web design and development projects.
-          </div>
-          <div className={classNames(styles.feed, styles.feed03)}>
-            ...
-          </div>
+        <div className={styles.logo}>
+          <OverviewLogoSvg />
         </div>
-        <div className={styles.footer}>
-          <div className={styles.join}>
-            <Link href="mailto:xxx@xxx.xxx" className={styles.link}>Email</Link>
-            <Link href="https://www.linkedin.com/in/annrra/" className={styles.link}>LinkedIn</Link>
-            <Link href="https://github.com/annrra" className={styles.link}>GitHub</Link>
-          </div>
-          <div className={styles.copy}>Created by BetterMonday - 2026</div>
+        <div className={classNames(styles.feed, styles.feed01)}>
+          I am a UX designer / developer and occasional photographer from Bulgaria currently living in Sofia. Here at bettermonday can be found some of the stuff over which I've been working lately. My recent work consists mostly of design, Front-end development and its implementation into different content management systems.
         </div>
-      </div>
+        <div className={classNames(styles.feed, styles.feed02)}>
+          I like to keep things simple mixing up thoughtful research and attention for the detail in the process. I'm currently available for web design and development projects.
+        </div>
+        <div className={classNames(styles.feed, styles.feed03)}>
+          ...
+        </div>
     </OverViewPanel>
   );
 };
--- src/components/OverView/OverViewPanel.tsx
+++ src/components/OverView/OverViewPanel.tsx
@@ -1,11 +1,37 @@
 "use client";
 import React from 'react';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { AnimatePresence, motion } from 'framer-motion';
 import styles from './ov.module.css';
 
 type OverViewPanelProps = {
   children: React.ReactNode;
+};
+
+const containerVariants = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.18,
+      delayChildren: 0.25, // wait for panel
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 34,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: [0.22, 1, 0.36, 1] as const,
+    },
+  },
 };
 
 const OverViewPanel: React.FC<OverViewPanelProps> = ({ children }) => {
@@ -26,7 +52,34 @@ const OverViewPanel: React.FC<OverViewPanelProps> = ({ children }) => {
           ease: [0.77, 0, 0.175, 1],
         }}
       >
-        {children}
+        <motion.div 
+          className={styles.pane}
+          variants={containerVariants}
+          initial="hidden"
+          animate="show"
+        >
+          <Link 
+            href="/" 
+            className={styles.x} 
+          />
+          <motion.div 
+            className={styles.body}
+            variants={itemVariants}
+          >
+            {children}
+          </motion.div>
+          <motion.div 
+            className={styles.footer}
+            variants={itemVariants}
+          >
+            <div className={styles.join}>
+              <Link href="mailto:xxx@xxx.xxx" className={styles.link}>Email</Link>
+              <Link href="https://www.linkedin.com/in/annrra/" className={styles.link}>LinkedIn</Link>
+              <Link href="https://github.com/annrra" className={styles.link}>GitHub</Link>
+            </div>
+            <div className={styles.copy}>Created by BetterMonday - 2026</div>
+          </motion.div>
+        </motion.div>
       </motion.div>
       )}
     </AnimatePresence>
