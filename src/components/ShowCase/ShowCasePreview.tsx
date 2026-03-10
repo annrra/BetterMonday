035f7b7444b501c322b6b0b51a1790b8b83fc28c
annrra <annrra@gmail.com>
2026-03-10 17:38:20 +0200

    Adjustments in animations for ShowCase preview cards and tooltip

--- src/components/ShowCase/ShowCasePreview.tsx
+++ src/components/ShowCase/ShowCasePreview.tsx
@@ -18,8 +18,8 @@ const ShowCasePreview = ({ media, cursor, visible }: ShowCasePreviewProps) => {
     <motion.div
       className={styles.preview}
       animate={{
-        x: cursor.x - PREVIEW_WIDTH - 90,
-        y: cursor.y - PREVIEW_HEIGHT - 60,
+        x: cursor.x - PREVIEW_WIDTH - 70,
+        y: cursor.y - PREVIEW_HEIGHT - 35,
         opacity: visible ? 1 : 0,
       }}
       transition={{
@@ -32,25 +32,25 @@ const ShowCasePreview = ({ media, cursor, visible }: ShowCasePreviewProps) => {
       <div className={classNames(styles.blob, styles.sm)}>
         <motion.span
           key={`sm-${media}`}
-          initial={{ scale: 1 }}
-          animate={{ scale: [1, 0.1, 1] }}
+          initial={{ scale: 0.6 }}
+          animate={{ scale: [0.6, 1.1, 1] }}
           transition={{ duration: 0.3, ease: "easeInOut", delay: 0 }}
         ></motion.span>
       </div>
       <div className={classNames(styles.blob, styles.lg)}>
         <motion.span
           key={`lg-${media}`}
-          initial={{ scale: 1 }}
-          animate={{ scale: [1, 0.1, 1] }}
-          transition={{ duration: 0.3, ease: "easeInOut", delay: 0.08 }}
+          initial={{ scale: 0.8 }}
+          animate={{ scale: [0.8, 1.1, 1] }}
+          transition={{ duration: 0.3, ease: "easeInOut", delay: 0.1 }}
         ></motion.span>
       </div>
       <motion.div
         key={`case-${media}`}
         className={styles.case}
-        initial={{ scale: 0.99, opacity: 0.85 }}
-        animate={{ scale: [0.99, 1.01, 1], opacity: [0.85, 1, 1] }}
-        transition={{ duration: 0.3, ease: "easeOut", delay: 0.16 }}
+        initial={{ scale: 0.98, opacity: 0.85 }}
+        animate={{ scale: [0.98, 1.02, 1], opacity: [0.85, 1, 1] }}
+        transition={{ duration: 0.3, ease: "easeOut", delay: 0.2 }}
       >
         {media ? (
           <Image
