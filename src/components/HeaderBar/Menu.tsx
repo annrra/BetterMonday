cd06fef4f50cae25e413b237e8b88dcacf924a6b
annrra <annrra@gmail.com>
2026-03-28 17:49:19 +0200

    Highlight active page link in navigation

--- src/components/HeaderBar/Menu.tsx
+++ src/components/HeaderBar/Menu.tsx
@@ -1,22 +1,30 @@
-"use client";
+'use client';
 import { TransitionLink } from '@/src/components/transitions';
 import { ComingSoonTooltip } from '@/src/components/_utils/ComingSoonTooltip';
 import styles from './hb.module.css';
 import classNames from 'classnames';
+import { usePathname } from 'next/navigation';
 
 type MenuProps = {
   mode?: 'default' | 'light';
 };
 
 const Menu = ( { mode = 'default' }: MenuProps ) => {
+  const pathname = usePathname(); // current route
 
   return (
     <nav className={classNames(styles.nav, { [styles.light]: mode === 'light' })}>
-      <TransitionLink href="/about">
+      <TransitionLink 
+        href="/about"
+        className={classNames({ [styles.active]: pathname === '/about' })}
+      >
         <span>about</span>
       </TransitionLink>
       <ComingSoonTooltip message='Under development..'>
-        <TransitionLink href="/work">
+        <TransitionLink 
+          href="/work"
+          className={classNames({ [styles.active]: pathname === '/work' })}
+        >
           <span>work</span>
         </TransitionLink>
       </ComingSoonTooltip>
