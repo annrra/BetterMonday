f2d5d2f01fb953566e1809f03b42970974e08ea4
annrra <annrra@gmail.com>
2026-03-31 19:21:04 +0300

    Code cleanup

--- src/components/ShowCase/index.ts
+++ src/components/ShowCase/index.ts
@@ -1,4 +1,3 @@
-export { default as ShowCaseClient } from './ShowCaseClient';
 export { default as ShowCaseLayout } from './ShowCaseLayout';
 export { default as ShowCaseLayoutFooter } from './ShowCaseLayoutFooter';
 export { default as ShowCaseServer } from './ShowCaseServer';
